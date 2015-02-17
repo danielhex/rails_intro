2bc030614300076d5ac027e893e90b8f5fff79e4
danielhex <danielhex@berkeley.edu>
2015-02-17 14:16:35 -0800

    rough implementation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,10 @@
 class Movie < ActiveRecord::Base
   attr_accessible :title, :rating, :description, :release_date
+  def self.get_ratings
+  	result = {}
+  	self.select(:rating).uniq.each do |movie|
+  		result[movie.rating] = 1
+  	end
+  	return result
+  end
 end
