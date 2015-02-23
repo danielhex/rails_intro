a88398fbb5f6ccf256e1111354e01a5721493b2f
danielhex <danielhex@berkeley.edu>
2015-02-23 08:47:02 -0800

    ?

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -41,6 +41,10 @@ Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
 end
 
+When /^(?:|I )press "([^"]*)"$/ do |button|
+  click_button(button)
+end
+
 # When /^(?:|I )check "([^"]*)"$/ do |field|
 #   check(field)
 # end
