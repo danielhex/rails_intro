370e64d8337a77974c3fccd703a09a4a44b71581
danielhex <danielhex@berkeley.edu>
2015-02-23 08:43:27 -0800

    fix

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -24,6 +24,17 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(',').each do |rating|
+    #flunk uncheck
+    if uncheck
+      #When %{I fill in "#{name}" with "#{value}"}
+      #When /^(?:|I )check "([^"]*)"$/ do |field|
+      uncheck("ratings_#{rating}")
+      #end
+    else
+      check("ratings_#{rating}")
+    end
+  end
 end
 
 Then /I should see all the movies/ do
@@ -37,20 +48,3 @@ end
 # When /^(?:|I )uncheck "([^"]*)"$/ do |field|
 #   uncheck(field)
 # end
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  #puts uncheck
-  rating_list.split(',').each do |rating|
-    #flunk uncheck
-    if uncheck
-      #When %{I fill in "#{name}" with "#{value}"}
-      #When /^(?:|I )check "([^"]*)"$/ do |field|
-      uncheck("ratings_#{rating}")
-      #end
-    else
-      check("ratings_#{rating}")
-    end
-  end
-end
